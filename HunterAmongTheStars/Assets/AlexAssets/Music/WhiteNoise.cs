5605a6a3ccca2e6f43536d245b09da8ec557562b
AlexandrTimchenko <aleksandar.timchenko@student.saedubai.com>
2024-12-16 02:10:23 +0900

    Last version
    New UI, New enemies, Logic, Winnig conditions

--- HunterAmongTheStars/Assets/AlexAssets/Music/WhiteNoise.cs
+++ HunterAmongTheStars/Assets/AlexAssets/Music/WhiteNoise.cs
@@ -2,35 +2,51 @@ using UnityEngine;
 
 public class DynamicAudio : MonoBehaviour
 {
-    public Transform player; // Ссылка на игрока
-    public float minDistance = 5f; // Минимальное расстояние
-    public float maxDistance = 20f; // Максимальное расстояние
+    public Transform player; 
+      
+
+    public float minDistance = 5f; 
+
+    public float maxDistance = 20f; 
+
     private AudioSource audioSource;
 
     void Start()
     {
+
         audioSource = GetComponent<AudioSource>();
+
     }
 
     void Update()
     {
-        // Вычисляем расстояние до игрока
+         
         float distance = Vector3.Distance(player.position, transform.position);
 
-        // Рассчитываем громкость на основе расстояния
+          
         if (distance <= minDistance)
         {
-            audioSource.volume = 1f; // Максимальная громкость
+
+            audioSource.volume = 1f;  //max vol
+
         }
         else if (distance >= maxDistance)
         {
-            audioSource.volume = 0f; // Звук не слышен
+
+            audioSource.volume = 0f; 
+
         }
         else
         {
-            // Пропорциональная громкость между minDistance и maxDistance
+            
             float t = (distance - minDistance) / (maxDistance - minDistance);
+             
             audioSource.volume = Mathf.Lerp(1f, 0f, t);
+
         }
+
+
     }
+
+     
 }
